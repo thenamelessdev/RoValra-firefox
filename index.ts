03f3a86c166de6d7bd75191fab6ce6c59175a47d
thenamelessdev <aron.minarovits@gmail.com>
2026-08-31 23:21:45 +0200

    feat: make script that converts extension

---
+++ index.ts
@@ -0,0 +1,26 @@
+// this expects RoValra to be cloned in the RoValra folder
+// also run this is a linux environment
+
+import path from "path"
+import fs from "fs/promises"
+import {exec} from "child_process"
+
+const roValraPath = path.join(process.cwd(), "RoValra")
+
+fs.access(roValraPath, fs.constants.F_OK).catch(() => {
+    throw new Error("No RoValra in RoValra/")
+})
+
+exec(`sed -i 's/chrome\./browser\./g' ${path.join(roValraPath, "src/**/*.js")}`, (error) => {
+    if (error) {
+        console.error(error)
+    }
+})
+
+const manifestPath = path.join(roValraPath, "manifest.json");
+
+let manifestFile = JSON.parse(await fs.readFile(manifestPath, "utf-8"));
+
+manifestFile.background = {"scripts": ["background.js"]}
+
+await fs.writeFile(manifestPath, JSON.stringify(manifestFile))
