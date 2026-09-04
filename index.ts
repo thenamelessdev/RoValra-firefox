de261b501b96b685d7cd4925b94b4f4ea72a6355
thenamelessdev <aron.minarovits@gmail.com>
2026-09-04 20:25:34 +0200

    fix: restructure browser_specific_settings in manifest file

--- index.ts
+++ index.ts
@@ -24,7 +24,11 @@ let manifestFile = JSON.parse(await fs.readFile(manifestPath, "utf-8"));
 manifestFile.optional_permissions = manifestFile.optional_permissions.filter((p: string) => p !== "contextMenus" && p !== "menus")
 
 manifestFile.background = {"scripts": ["background.js"]}
-manifestFile.browser_specific_settings.gecko.id = "rovalra@thenamelessdev.com"
-manifestFile.browser_specific_settings.gecko.strict_min_version = "109.0"
+manifestFile.browser_specific_settings = {
+    "gecko": {
+        "id": "rovalra@thenamelessdev.com",
+        "strict_min_version": "109.0"
+    }
+}
 
 await fs.writeFile(manifestPath, JSON.stringify(manifestFile))
